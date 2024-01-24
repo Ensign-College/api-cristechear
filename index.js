6f5e5b517f679b37c2bb7c51cb267ecf0ae019d4
Artur <arthurcristeche@gmail.com>
2024-01-24 09:55:13 -0700

    Added boxes

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
+const Redis = require('redis');//redis is a key-value store
+
 const express = require('express');//express makes APIs - connect frontend to server
+// Connect Redis
+const client = Redis.createClient({
+    url: 'redis://localhost:6379'
+  });
+client.on('error', err => console.log('Redis Client Error', err));
+
+
+
 const app = express();//create express app
-app.listen(3000);//listen on port 3000
+const port = 3000;//set port
+
 console.log('hello world');
 
-const boxes = [
-    {boxId:1},
-    {boxId:2},
-    {boxId:3},
-    {boxId:4}
-];
 //1st parameter url
 //2nd a function to return boxes
 //req= the request from the browser
 //res= the response from the server
+
 app.get('/boxes', (req, res) => {
-    res.send(JSON.stringify(boxes));//send boxes as a string
-    
+    let boxes = client.json.get('boxes',{path:'$'});//get boxes from redis
+
+    res.send(JSON.stringify(boxes));//send boxes as a string to the browser
+
 })
+app.listen(port, () => {
+    client.connect();
+    console.log(`Listening on port ${port}`);
+});//listen on port 3000
