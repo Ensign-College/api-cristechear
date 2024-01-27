7fdca6a2dad2d8d53a9d4b0fcb767af318153fe7
Artur <arthurcristeche@gmail.com>
2024-01-26 20:32:50 -0700

    Connect the API to Redis: Creating an API layer of an application

--- index.js
+++ index.js
@@ -1,31 +1,43 @@
-const Redis = require('redis');//redis is a key-value store
-
-const express = require('express');//express makes APIs - connect frontend to server
+const Redis = require("redis"); //redis is a key-value store
+const bodyParser = require("body-parser"); //this will transform te order request from json to the data base
+const express = require("express"); //express makes APIs - connect frontend to server
 // Connect Redis
 const client = Redis.createClient({
-    url: 'redis://localhost:6379'
-  });
-client.on('error', err => console.log('Redis Client Error', err));
+  url: "redis://localhost:6379",
+});
+client.on("error", (err) => console.log("Redis Client Error", err));
 
+const app = express(); //create express app
+const port = 3000; //set port
 
-
-const app = express();//create express app
-const port = 3000;//set port
-
-console.log('hello world');
+console.log("hello world");
 
 //1st parameter url
 //2nd a function to return boxes
 //req= the request from the browser
 //res= the response from the server
+app.use(bodyParser.json());
 
-app.get('/boxes', (req, res) => {
-    let boxes = client.json.get('boxes',{path:'$'});//get boxes from redis
+app.get("/boxes", (req, res) => {
+  let boxes = client.json.get("boxes", { path: "$" }); //get boxes from redis
 
-    res.send(JSON.stringify(boxes));//send boxes as a string to the browser
+  res.send(JSON.stringify(boxes)); //send boxes as a string to the browser
+});
 
-})
+app.post('/boxes', async (req, res) => { //async means we will await promises
+
+    console.log(req.body);
+    const newBox = req.body;
+//   const newBox = {
+//     boxId: req.body.boxId,
+//   };
+//   client.json.set('boxes', { path: '$', JSON.stringify(newBox) }) or
+await client.json.arrAppend('boxes', '$',newBox) //add new box to boxes array in redis (req.body)
+
+res.json(newBox)
+});
+
 app.listen(port, () => {
-    client.connect();
-    console.log(`Listening on port ${port}`);
-});//listen on port 3000
+  client.connect();
+  console.log(`Listening on port ${port}`);
+}); //listen on port 3000
